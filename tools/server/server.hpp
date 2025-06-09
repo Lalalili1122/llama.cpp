ae440ec9fa33558dda5305d7d7387d506f9b0740
Lucas <lpawlak@zonebourse.com>
2025-06-09 16:57:32 +0200

    remove all the deprecate stuff

--- tools/server/server.hpp
+++ tools/server/server.hpp
@@ -5,6 +5,10 @@
 // mime type for sending response
 #define MIMETYPE_JSON "application/json; charset=utf-8"
 
+bool has_extension(const std::string& filename, const std::string& ext);
+std::string read_file(const std::string& filepath);
+std::string extract_pdf_text(const std::string& filepath);
+
 // ENUM
 enum stop_type {
     STOP_TYPE_NONE,
@@ -1944,7 +1948,8 @@ struct server_context {
             }
         }
 
-        if (!llama_kv_self_can_shift(ctx)) {
+        llama_memory_t mem = llama_get_memory(ctx);
+        if (!llama_memory_can_shift(mem)) {
             if (params_base.ctx_shift) {
                 params_base.ctx_shift = false;
                 SRV_WRN("%s\n", "ctx_shift is not supported by this context, it will be disabled");
@@ -2163,7 +2168,11 @@ struct server_context {
         SRV_DBG("%s", "clearing KV cache\n");
 
         // clear the entire KV cache
-        llama_kv_self_clear(ctx);
+        llama_memory_t kv = llama_get_memory(ctx);
+        if (!kv) {
+                return;
+        }
+        llama_memory_clear(kv, true);
         clean_kv_cache = false;
     }
 
@@ -2849,7 +2858,12 @@ struct server_context {
 
                     // Erase token cache
                     const size_t n_erased = slot->cache_tokens.size();
-                    llama_kv_self_seq_rm(ctx, slot->id, -1, -1);
+                    
+                    llama_memory_t kv = llama_get_memory(ctx);
+                    if (kv) {
+                        llama_memory_seq_rm(kv, slot->id, -1, -1);
+                    }
+
                     slot->cache_tokens.clear();
 
                     auto res = std::make_unique<server_task_result_slot_erase>();
@@ -2924,8 +2938,13 @@ struct server_context {
 
                 SLT_WRN(slot, "slot context shift, n_keep = %d, n_left = %d, n_discard = %d\n", n_keep, n_left, n_discard);
 
-                llama_kv_self_seq_rm (ctx, slot.id, n_keep            , n_keep + n_discard);
-                llama_kv_self_seq_add(ctx, slot.id, n_keep + n_discard, slot.n_past,        -n_discard);
+                llama_memory_t kv = llama_get_memory(ctx);
+                if (kv) {
+                    llama_memory_seq_rm(kv, slot.id, n_keep, n_keep + n_discard);
+                    llama_memory_seq_add(kv, slot.id, n_keep + n_discard, slot.n_past, -n_discard);   
+                }
+
+
 
                 // add generated tokens to cache
                 {
@@ -3128,8 +3147,12 @@ struct server_context {
 
                                             const int64_t kv_shift = (int64_t) head_p - (int64_t) head_c;
 
-                                            llama_kv_self_seq_rm (ctx, slot.id, head_p, head_c);
-                                            llama_kv_self_seq_add(ctx, slot.id, head_c, head_c + n_match, kv_shift);
+                                            llama_memory_t kv = llama_get_memory(ctx);
+                                            if (kv) {
+                                                llama_memory_seq_rm (kv, slot.id, head_p, head_c);
+                                                llama_memory_seq_add(kv, slot.id, head_c, head_c + n_match, kv_shift);
+                                            }
+
 
                                             for (size_t i = 0; i < n_match; i++) {
                                                 slot.cache_tokens.set_token(head_p + i, slot.cache_tokens[head_c + i]);
@@ -3151,7 +3174,12 @@ struct server_context {
                             }
 
                             if (slot.n_past > 0 && slot.n_past < (int) slot.cache_tokens.size()) {
-                                const auto pos_min = llama_kv_self_seq_pos_min(ctx, slot.id);
+                                llama_memory_t kv = llama_get_memory(ctx);
+                                auto pos_min = 0;
+                                if (kv) {
+                                    pos_min = llama_memory_seq_pos_min(kv, slot.id);
+                                }
+
                                 if (pos_min == -1) {
                                     SLT_ERR(slot, "n_past = %d, cache_tokens.size() = %d, seq_id = %d, pos_min = %d\n", slot.n_past, (int) slot.cache_tokens.size(), slot.id, pos_min);
                                     GGML_ABORT("pos_min == -1, but n_past > 0 - should not happen: https://github.com/ggml-org/llama.cpp/pull/13833#discussion_r2116181237");
@@ -3186,13 +3214,19 @@ struct server_context {
                     }
 
                     // keep only the common part
-                    if (!llama_kv_self_seq_rm(ctx, slot.id, slot.n_past, -1)) {
-                        // could not partially delete (likely using a non-Transformer model)
-                        llama_kv_self_seq_rm(ctx, slot.id, -1, -1);
+                    llama_memory_t kv = llama_get_memory(ctx);
+                    bool ok = true;
+                    if (kv) {
+                        ok = llama_memory_seq_rm(kv, slot.id, slot.n_past, -1);
+                    }
 
-                        // there is no common part left
+                    if (!ok) {
+                        if (kv) {
+                            llama_memory_seq_rm(kv, slot.id, -1, -1);
+                        }
                         slot.n_past = 0;
                     }
+
 
                     SLT_INF(slot, "kv cache rm [%d, end)\n", slot.n_past);
 
@@ -3528,7 +3562,11 @@ struct server_context {
                 slot.cache_tokens.push_back(id);
                 slot.cache_tokens.insert({ids.begin(), ids.end() - 1});
 
-                llama_kv_self_seq_rm(ctx, slot.id, slot.n_past, -1);
+                llama_memory_t kv = llama_get_memory(ctx);
+                if (kv) {
+                    llama_memory_seq_rm(kv, slot.id, slot.n_past, -1);
+                }
+
 
                 for (size_t i = 0; i < ids.size(); ++i) {
                     completion_token_output result;
